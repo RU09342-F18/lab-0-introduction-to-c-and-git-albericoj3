807dcd1f8a0dffcce3ee3d6a42f2ee46e94b3691
albericoj3 <36242632+albericoj3@users.noreply.github.com>
2018-09-07 16:51:13 -0400

    Update math.h
    Added addition and subtraction for testing

--- Assignment/math.h
+++ Assignment/math.h
@@ -27,3 +27,17 @@
 int math(int num1, int num2, char Operator);
 
 #endif /* MATH_H_ */
+
+int math(int num1, int num2, char Operator)
+{
+  switch(Operator)
+  {
+    case +: return(num1 + num2);
+      break;
+    case -: return(num1 - num2);
+      break;
+    default: printf("Invalid Operator");
+      break;
+  }
+  return 0;
+}
