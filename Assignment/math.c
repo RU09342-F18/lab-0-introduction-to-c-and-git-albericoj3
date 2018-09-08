5800ea4a1cd8cb47227bb53c92b718e43f66fc99
albericoj3 <36242632+albericoj3@users.noreply.github.com>
2018-09-08 12:01:58 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,9 +1,23 @@
 /*
-*
-*
-*
+* math.c
+* Author: Jordan Alberico
+* Created on: 9/8/18
+* Last edited: 9/8/18
 */
 
 
 #include<math.h>
 
+int math(int num1, int num2, char Operator)
+{
+  switch(Operator)
+  {
+    case +: return(num1 + num2);
+      break;
+    case -: return(num1 - num2);
+      break;
+    default: printf("Invalid Operator");
+      break;
+  }
+  return 0;
+}
